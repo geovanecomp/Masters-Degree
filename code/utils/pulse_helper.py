e8748dd5efc7ddaf9aa30421f1fc25c9a46385aa
Geovane Pacheco <geovane.pacheco99@gmail.com>
2020-07-26 22:53:48 -0300

    Pulse and File helper

---
+++ code/utils/pulse_helper.py
@@ -0,0 +1,48 @@
+"""This helper provides different deformation pulses"""
+
+import numpy as np
+
+
+# To be implemented
+def get_pulse_paper_COF():
+    pass
+
+
+# Generates a deformation pulse. It can represents for example hardware aging.
+def get_jitter_pulse():
+    base_pulse = np.loadtxt('data/pulsehi_physics.txt')
+    pulsehi = []  # Stores the deformation pulse
+    pulse_dim = 2
+    pulse_idx = 1
+    pulse_bandwidth = 150  # In nanoseconds
+    reading_interval = 25  # In nanoseconds
+    number_of_pulses = int(pulse_bandwidth / reading_interval) + 1  # WHY?
+
+    # Sum of each side of the Normal Distribution
+    step = reading_interval + reading_interval
+    reading_position = pulse_bandwidth * (-1)
+
+    pulse = np.concatenate(
+                (
+                    np.zeros((pulse_bandwidth, pulse_dim)),
+                    base_pulse,
+                    np.zeros((pulse_bandwidth, pulse_dim))
+                )
+            )
+
+    # jitter = np.random.randint(-6, 6, 1) # Phase deviation
+    jitter = 0  # Phase deviation - It is set as 0 for now...
+
+    # mu, sigma = 0, 0.05 # mean and standard deviation
+    # Represents hardware agin or general uncertainty
+    # deformation = np.random.normal(mu, sigma, number_of_pulses)
+    deformation = np.zeros(number_of_pulses)  # It is set as 0 for now...
+
+    zero_idx = np.where(pulse[:, 0] < 0)
+    zero_idx = zero_idx[0][-1] + 1
+
+    for i in range(0, number_of_pulses):
+        pulsehi.append(pulse[zero_idx + reading_position + jitter, pulse_idx] + deformation[i])
+        reading_position = reading_position + step
+
+    return pulsehi

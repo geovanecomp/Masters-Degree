b859d18e952cd66ca4a8a427dd492c33467d9fe9
Geovane Pacheco <geovane.pacheco99@gmail.com>
2020-08-22 19:35:03 -0300

    OF PU comparison added

--- code/plots/of_histograms_comparison.py
+++ code/plots/of_histograms_comparison.py
@@ -1,35 +1,50 @@
-"""Process Linear plot comparing two datasets."""
+"""Compares OF Amplitude against OF Error (OF Amplitude - Amplitude)."""
 
 import os.path
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 DIR_PATH = os.path.dirname(__file__)
 
 if __name__ == '__main__':
-    num_events = 1000
+    prob = 0.0
+    num_events = 10000
 
-    amp_error = pd.read_csv(DIR_PATH + '/../results/optimal_filter/{}-events/amp_error.txt'.format(num_events), sep=" ", header=None).T
-    of_amplitude = pd.read_csv(DIR_PATH + '/../results/optimal_filter/{}-events/of_amplitude.txt'.format(num_events), sep=" ", header=None).T
+    # Normal data
+    # amp_error_file_name = DIR_PATH + '/../results/optimal_filter/{}_events/of_amp_error.txt'.format(num_events)
+    # of_amplitude_file_name = DIR_PATH + '/../results/optimal_filter/{}_events/of_amplitude.txt'.format(num_events)
+
+    # Pile data
+    amp_error_file_name = DIR_PATH + '/../results/optimal_filter/{}_events/pileup_prob_{}_of_amp_error.txt'.format(num_events, prob)
+    of_amplitude_file_name = DIR_PATH + '/../results/optimal_filter/{}_events/pileup_prob_{}_of_amplitude.txt'.format(num_events, prob)
+
+    amp_error = np.loadtxt(amp_error_file_name).T
+    of_amplitude = np.loadtxt(of_amplitude_file_name).T
+
+    amp_error_mean = np.mean(amp_error)
+    amp_error_std = np.std(amp_error)
+
+    of_amplitude_mean = np.mean(of_amplitude)
+    of_amplitude_std = np.std(of_amplitude)
 
     fig, ((ax0, ax1)) = plt.subplots(nrows=1, ncols=2)
 
-    fig.suptitle('Comparação do OF Data X Error \n' '{} eventos'
-                 .format(num_events))
+    fig.suptitle('OF: Comparação da Amplitude X Erro \n' '{} eventos e Empilhamento de {}%'
+                 .format(num_events, prob))
     ax0.hist(of_amplitude, bins="auto")
     ax0.legend(prop={'size': 10})
     ax0.grid(axis='y', alpha=0.75)
-    ax0.set_title('OF Data \n' r'$\mu={}$, $\sigma={}$'
-                  .format(of_amplitude.mean(axis=1)[0], of_amplitude.std(axis=1)[0]))
+    ax0.set_title('Amplitude \n' r'$\mu={}$, $\sigma={}$'
+                  .format(of_amplitude_mean, of_amplitude_std))
     ax0.set_xlabel('Valor')
     ax0.set_ylabel('Frequência')
 
     ax1.hist(amp_error, bins="auto")
     ax1.legend(prop={'size': 10})
     ax1.grid(axis='y', alpha=0.75)
-    ax1.set_title('OF Error \n' r'$\mu={}$, $\sigma={}$'
-                  .format(amp_error.mean(axis=1)[0], amp_error.std(axis=1)[0]))
+    ax1.set_title('Erro \n' r'$\mu={}$, $\sigma={}$'
+                  .format(amp_error_mean, amp_error_std))
     ax1.set_xlabel('Valor')
     ax1.set_ylabel('Frequência')
 

08c470b62b9e005a0003c866888f4fd16f868296
Geovane Pacheco <geovane.pacheco99@gmail.com>
2020-11-01 18:06:52 -0300

    Plots customization to consider real data

--- code/plots/of_mf_histograms_error_comparison.py
+++ code/plots/of_mf_histograms_error_comparison.py
@@ -4,19 +4,24 @@ import os.path
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rcParams['xtick.labelsize'] = 22
-plt.rcParams['ytick.labelsize'] = 22
+# plt.rcParams['xtick.labelsize'] = 22
+# plt.rcParams['ytick.labelsize'] = 22
 
 DIR_PATH = os.path.dirname(__file__)
 
 if __name__ == '__main__':
-    prob = 50.0
-    num_events = 10000
 
-    # Pile data
-    amplitude_file_name = DIR_PATH + '/../results/pileup_data/prob_{}/{}_events/tile_A_signal_prob_{}.txt'.format(prob, num_events, prob)
-    of_amp_error_file_name = DIR_PATH + '/../results/optimal_filter/{}_events/pileup_prob_{}_of_amp_error.txt'.format(num_events, prob)
-    mf_amp_error_file_name = DIR_PATH + '/../results/matched_filter/{}_events/pileup_prob_{}_amp_error.txt'.format(num_events, prob)
+    # # Pile data
+    # prob = 50.0/
+    # amplitude_file_name = DIR_PATH + '/../results/pileup_data/prob_{}/{}_events/tile_A_signal_prob_{}.txt'.format(prob, num_events, prob)
+    # of_amp_error_file_name = DIR_PATH + '/../results/optimal_filter/{}_events/pileup_prob_{}_of_amp_error.txt'.format(num_events, prob)
+    # mf_amp_error_file_name = DIR_PATH + '/../results/matched_filter/{}_events/pileup_prob_{}_amp_error.txt'.format(num_events, prob)
+
+    # Real data
+    noise_mean = 30
+    amplitude_file_name = DIR_PATH + '/../results/real_data/mu{}/tile_A.txt'.format(noise_mean)
+    of_amp_error_file_name = DIR_PATH + '/../results/real_data/mu{}/optimal_filter/of_amp_error.txt'.format(noise_mean)
+    mf_amp_error_file_name = DIR_PATH + '/../results/real_data/mu{}/matched_filter/mf_amp_error.txt'.format(noise_mean)
 
     amplitude = np.loadtxt(amplitude_file_name)
     of_amp_error = np.loadtxt(of_amp_error_file_name)
@@ -25,25 +30,25 @@ if __name__ == '__main__':
     fig, ((ax0, ax1)) = plt.subplots(nrows=1, ncols=2)
     font = {
             'family': 'Times New Roman',
-            'size': 22
+            'size': 16
             }
 
-    # fig.suptitle('Comparação da Amplitude Verdadeira X Numérica \n' '{} eventos e Empilhamento de {}%'
+    # fig.suptitle('Comparação do erro \n' '{} eventos e Empilhamento de {}%'
     #              .format(num_events, prob))
-
-    ax0.hist(of_amp_error, bins="auto", facecolor='k')
+    fig.suptitle('Comparação do erro')
+    ax0.hist(of_amp_error, bins="auto", color='dimgrey')
     ax0.legend(prop={'size': 10})
     ax0.grid(axis='y', alpha=0.75)
-    # ax0.set_title('OF ' r'$\mu={}$, $\sigma={}$'
-    #               .format(of_amp_error.mean(), of_amp_error.std()))
+    ax0.set_title('OF \n' r'$\mu={}$, $\sigma={}$'
+                  .format(of_amp_error.mean(), of_amp_error.std()))
     ax0.set_xlabel('Erro de Estimação', **font)
     ax0.set_ylabel('Eventos', **font)
 
-    ax1.hist(mf_amp_error, bins="auto", facecolor='k')
+    ax1.hist(mf_amp_error, bins="auto", facecolor='dimgrey')
     ax1.legend(prop={'size': 10})
     ax1.grid(axis='y', alpha=0.75)
-    # ax1.set_title('MF ' r'$\mu={}$, $\sigma={}$'
-    #               .format(mf_amp_error.mean(), mf_amp_error.std()))
+    ax1.set_title('MF \n' r'$\mu={}$, $\sigma={}$'
+                  .format(mf_amp_error.mean(), mf_amp_error.std()))
     ax1.set_xlabel('Erro de Estimação', **font)
     ax1.set_ylabel('Eventos', **font)
 

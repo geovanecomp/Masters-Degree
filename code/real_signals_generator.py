6144105845eb8a5e56534b3fcfe9a874843d06e5
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-01-03 19:08:49 -0300

    Message improvement

--- code/real_signals_generator.py
+++ code/real_signals_generator.py
@@ -9,7 +9,7 @@ def real_signals_generator(real_noises, noise_mean):
     dimension = 7
     amplitude_mean = 300  # Exponential signal mean
     number_of_events = len(real_noises)
-    print('Real Signal Generator')
+    print('Real Signal Generator using mean: {}'.format(noise_mean))
 
     folder_name = 'real_data/mu{}'.format(noise_mean)
 
@@ -27,6 +27,6 @@ def real_signals_generator(real_noises, noise_mean):
 if __name__ == '__main__':
     noise_mean = 90
     tile_partition = 'LBA'
-    real_noise_file_name = f'data/{tile_partition}/{tile_partition}mu{noise_mean}.txt'
+    real_noise_file_name = f'data/{tile_partition}/{tile_partition}mu{noise_mean}_no_ped.txt'
     real_noises = pd.read_csv(real_noise_file_name, sep=" ", usecols=(3, 4, 5 ,6, 7, 8, 9), header=None)
     real_signals_generator(real_noises, noise_mean)

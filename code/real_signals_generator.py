74174a56a27de16cec83e215398514704eb54b11
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-01-24 12:06:23 -0300

    Adds sufix approach for different datasets

--- code/real_signals_generator.py
+++ code/real_signals_generator.py
@@ -5,13 +5,13 @@ import pandas as pd
 from utils import file_helper, pulse_helper
 
 
-def real_signals_generator(real_noises, noise_mean):
+def real_signals_generator(real_noises, noise_mean, sufix=''):
     dimension = 7
-    amplitude_mean = 300  # Exponential signal mean
+    amplitude_mean = 30  # Exponential signal mean
     number_of_events = len(real_noises)
-    print('Real Signal Generator using mean: {}'.format(noise_mean))
+    print(f'Real Signal Generator using mean: {noise_mean}{sufix}')
 
-    folder_name = 'real_data/mu{}'.format(noise_mean)
+    folder_name = f'results/real_data/mu{noise_mean}'
 
     A = np.zeros(number_of_events)  # Amplitude
     data = np.zeros((number_of_events, dimension))
@@ -20,13 +20,14 @@ def real_signals_generator(real_noises, noise_mean):
         data[i, :] = real_noises.values[i][:] + \
             np.multiply(A[i], pulse_helper.get_jitter_pulse())
 
-    file_helper.save_file_in('tile_signal', folder_name, data)
-    file_helper.save_file_in('tile_A', folder_name, A)
+    file_helper.save_file_in(f'tile_signal{sufix}', folder_name, data)
+    file_helper.save_file_in(f'tile_A{sufix}', folder_name, A)
 
 
 if __name__ == '__main__':
     noise_mean = 90
+    sufix = '_small'
     tile_partition = 'LBA'
-    real_noise_file_name = f'data/{tile_partition}/{tile_partition}mu{noise_mean}_no_ped.txt'
-    real_noises = pd.read_csv(real_noise_file_name, sep=" ", usecols=(3, 4, 5 ,6, 7, 8, 9), header=None)
-    real_signals_generator(real_noises, noise_mean)
+    real_noise_file_name = f'data/{tile_partition}/{tile_partition}mu{noise_mean}{sufix}_no_ped.txt'
+    real_noises = pd.read_csv(real_noise_file_name, sep=" ", usecols=(3, 4, 5, 6, 7, 8, 9), header=None)
+    real_signals_generator(real_noises, noise_mean, sufix)

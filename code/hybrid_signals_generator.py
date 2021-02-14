a32828ec7503e34b852e9671a15955ca6df56842
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-02-14 00:20:07 -0300

    New sufix structure added

--- code/hybrid_signals_generator.py
+++ code/hybrid_signals_generator.py
@@ -5,10 +5,10 @@ import pandas as pd
 from utils import file_helper, pulse_helper
 
 
-def real_signals_generator(real_noises, noise_mean, sufix=''):
+def signals_generator(noises, noise_mean, sufix=''):
     dimension = 7
     amplitude_mean = 30  # Exponential signal mean
-    number_of_events = len(real_noises)
+    number_of_events = len(noises)
     print(f'Real Signal Generator using mean: {noise_mean}{sufix}')
 
     folder_name = f'results/hybrid/base_data/mu{noise_mean}'
@@ -17,7 +17,7 @@ def real_signals_generator(real_noises, noise_mean, sufix=''):
     data = np.zeros((number_of_events, dimension))
     for i in range(0, number_of_events):
         A[i] = np.random.exponential(amplitude_mean)  # Simulating true Amplitude
-        data[i, :] = real_noises.values[i][:] + \
+        data[i, :] = noises.values[i][:] + \
             np.multiply(A[i], pulse_helper.get_jitter_pulse())
 
     file_helper.save_file_in(f'tile_signal{sufix}', folder_name, data)
@@ -25,9 +25,10 @@ def real_signals_generator(real_noises, noise_mean, sufix=''):
 
 
 if __name__ == '__main__':
-    noise_mean = 90
-    sufix = '_small'
+    noise_mean = 30
+    channel = 24
+    sufix = f'_ch{channel}'
     tile_partition = 'LBA'
-    real_noise_file_name = f'data/{tile_partition}/{tile_partition}mu{noise_mean}{sufix}_no_ped.txt'
-    real_noises = pd.read_csv(real_noise_file_name, sep=" ", usecols=(3, 4, 5, 6, 7, 8, 9), header=None)
-    real_signals_generator(real_noises, noise_mean, sufix)
+    noise_file_name = f'data/{tile_partition}/{tile_partition}mu{noise_mean}_no_ped{sufix}.txt'
+    noises = pd.read_csv(noise_file_name, sep=" ", usecols=(3, 4, 5, 6, 7, 8, 9), header=None)
+    signals_generator(noises, noise_mean, sufix)

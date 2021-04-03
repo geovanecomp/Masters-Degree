ee053f12cbe48f73d86aac69ed6d2ed4efccdbb6
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-04-03 20:35:48 -0300

    Add sufix config for remove pedestal script

--- code/remove_pedestal.py
+++ code/remove_pedestal.py
@@ -9,14 +9,15 @@ from utils import tilecal_helper
 if __name__ == '__main__':
     tile_partition = 'EBA'
     noise_mean = 30
+    sufix = '_real'
 
-    print(f'Processing for mean: {tile_partition}_{noise_mean}')
+    print(f'Processing for mean: {tile_partition}_{noise_mean}{sufix}')
 
-    noise_file_name = f'data/{tile_partition}/{tile_partition}mu{noise_mean}.txt'
+    noise_file_name = f'data/{tile_partition}/{tile_partition}mu{noise_mean}{sufix}.txt'
     noises = np.loadtxt(noise_file_name)
     new_noises = tilecal_helper.generate_partition_data_without_ped(noises)
-    file_path = f'data/{tile_partition}/{tile_partition}mu{noise_mean}_no_ped.txt'
+    file_path = f'data/{tile_partition}/{tile_partition}mu{noise_mean}{sufix}_no_ped.txt'
 
     # 3 int digits for localization and others for the data
-    fmt = '%d', '%d', '%d', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f'
+    fmt = '%d', '%d', '%d', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f', '%.4f'
     np.savetxt(file_path, new_noises, fmt=fmt, delimiter=' ')

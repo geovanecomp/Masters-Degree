996307f3edd0e7e94377ff2217ba088d39f9ba5f
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-04-03 20:42:05 -0300

    Fix histograms bin range

--- code/plots/of_mf_histograms_error_comparison_v2.py
+++ code/plots/of_mf_histograms_error_comparison_v2.py
@@ -11,16 +11,16 @@ DIR_PATH = os.path.dirname(__file__)
 
 if __name__ == '__main__':
     # Real data
-    noise_mean = 30
-    channel = 24
+    noise_mean = 90
+    channel = 36
+    amplitude_mean = 100
+
     sufix = f'_ch{channel}'
-    base_folder = DIR_PATH + '/../results/hybrid'
-    amplitude_file_name = f'{base_folder}/base_data/mu{noise_mean}/tile_A{sufix}.txt'
+    base_folder = DIR_PATH + f'/../results/hybrid/amplitude_mean{amplitude_mean}'
     of_amp_error_file_name = f'{base_folder}/OF/mu{noise_mean}/of_amp_error{sufix}.txt'
     dmf_amp_error_file_name = f'{base_folder}/D_MF/mu{noise_mean}/dmf_amp_error{sufix}.txt'
     emf_amp_error_file_name = f'{base_folder}/E_MF/mu{noise_mean}/mf_amp_error{sufix}.txt'
 
-    amplitude = np.loadtxt(amplitude_file_name)
     of_amp_error = np.loadtxt(of_amp_error_file_name)
     dmf_amp_error = np.loadtxt(dmf_amp_error_file_name)
     emf_amp_error = np.loadtxt(emf_amp_error_file_name)
@@ -31,11 +31,9 @@ if __name__ == '__main__':
         print(f'D-MF Amp len: {len(dmf_amp_error)}')
         print(f'E-MF Amp len: {len(emf_amp_error)}')
 
-    # of_amp_error = of_amp_error[0:10000]
-    # mf_amp_error = mf_amp_error[0:10000]
-    bins = 500
+    bins = 200
 
-    fig, ((ax0)) = plt.subplots(nrows=1, ncols=1)
+    fig, ((ax0)) = plt.subplots(nrows=1, ncols=1, figsize=(19,10))
     font = {
             'family': 'Times New Roman',
             'size': 16
@@ -43,10 +41,12 @@ if __name__ == '__main__':
 
     # fig.suptitle('Comparação do erro \n' '{} eventos e Empilhamento de {}%'
     #              .format(num_events, prob))
-    fig.suptitle(f'Comparação do erro com Ruído Médio = {noise_mean} Canal: {channel}\n {len(of_amp_error)} eventos')
-    ax0.hist(of_amp_error, bins=bins, color='dimgrey', histtype=u'step', label='OF')
-    ax0.hist(dmf_amp_error, bins=bins, facecolor='dimgrey', histtype=u'step', label='D-MF')
-    ax0.hist(emf_amp_error, bins=bins, facecolor='dimgrey', histtype=u'step', label='E-MF')
+    fig.suptitle(f'Comparação do erro com Ruído Médio = {noise_mean} Canal: {channel} Eventos: {len(of_amp_error)}')
+
+    # ax0.hist(of_amp_error, bins=bins, range=(-75, 75), color='red', histtype=u'step', label='OF')
+    ax0.hist(of_amp_error, bins=bins, range=(-75, 75), color='red', histtype=u'step', label='OF')
+    ax0.hist(dmf_amp_error, bins=bins, range=(-75, 75), facecolor='dimgrey', histtype=u'step', label='D-MF')
+    ax0.hist(emf_amp_error, bins=bins, range=(-75, 75), facecolor='blue', histtype=u'step', label='E-MF')
     ax0.legend(prop={'size': 10})
     ax0.grid(axis='y', alpha=0.75)
     ax0.set_title('\nOF ' r'$\mu={}$, $\sigma={}$'
@@ -57,7 +57,5 @@ if __name__ == '__main__':
                           emf_amp_error.mean(), emf_amp_error.std()))
     ax0.set_xlabel('Erro de Estimação', **font)
     ax0.set_ylabel('Eventos', **font)
-    ax0.set_xlim(-100, 100)
-    # ax0.set_ylim(0, 5000000)
 
     plt.show()

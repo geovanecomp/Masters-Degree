e16e5798fb96cac10d3b8652e6c7d122d976e290
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-04-03 20:30:51 -0300

    Add sufix for channel data generation

--- code/generate_channel_data.py
+++ code/generate_channel_data.py
@@ -10,15 +10,16 @@ if __name__ == '__main__':
     tile_partition = 'EBA'
     noise_mean = 30
     channel = 10
+    sufix = '_real_no_ped'
 
     print(f'Processing for mean: {noise_mean}')
     print(f'Channel: {channel}')
 
-    noise_file_name = f'data/{tile_partition}/{tile_partition}mu{noise_mean}_no_ped.txt'
+    noise_file_name = f'data/{tile_partition}/{tile_partition}mu{noise_mean}{sufix}.txt'
     noises = np.loadtxt(noise_file_name)
     new_noises = tilecal_helper.generate_data_by_channel(noises, channel)
-    file_path = f'data/{tile_partition}/{tile_partition}mu{noise_mean}_no_ped_ch{channel}.txt'
+    file_path = f'data/{tile_partition}/{tile_partition}mu{noise_mean}{sufix}_ch{channel}.txt'
 
     # 3 int digits for localization and others for the data
-    fmt = '%d', '%d', '%d', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f'
+    fmt = '%d', '%d', '%d', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f', '%.6f', '%.4f'
     np.savetxt(file_path, new_noises, fmt=fmt, delimiter=' ')

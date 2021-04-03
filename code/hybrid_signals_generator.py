d7bbdb8ca8592b98d5d7c8a4c0e63e2d5011f8f1
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-04-03 20:31:36 -0300

    Saves jitter information during hybrid data generation

--- code/hybrid_signals_generator.py
+++ code/hybrid_signals_generator.py
@@ -5,23 +5,38 @@ import pandas as pd
 from utils import file_helper, pulse_helper
 
 
+def _save_jitter_file(file_name, folder_name, data):
+    jitter_file_fmt = '%d', '%.7f', '%.7f', '%.7f', '%.7f', '%.7f', '%.7f', '%.7f'
+    np.savetxt(
+        f'{folder_name}/{file_name}.txt',
+        data,
+        fmt=jitter_file_fmt,
+        delimiter=' ')
+
+
 def signals_generator(noises, noise_mean, sufix=''):
     dimension = 7
     amplitude_mean = 30  # Exponential signal mean
     number_of_events = len(noises)
-    print(f'Hybrid Signal Generator using mean: {noise_mean}{sufix}')
+    number_of_jitter_info = dimension + 1
+    print(f'Hybrid Signal Generator amp{amplitude_mean} using mean: {noise_mean}{sufix}')
 
-    folder_name = f'results/hybrid/base_data/mu{noise_mean}'
+    folder_name = f'results/hybrid/amplitude_mean{amplitude_mean}/base_data/mu{noise_mean}'
 
     A = np.zeros(number_of_events)  # Amplitude
+    jitters = np.zeros((number_of_events, number_of_jitter_info))
     data = np.zeros((number_of_events, dimension))
     for i in range(0, number_of_events):
         A[i] = np.random.exponential(amplitude_mean)  # Simulating true Amplitude
-        data[i, :] = noises.values[i][:] + \
-            np.multiply(A[i], pulse_helper.get_jitter_pulse())
+        jitter_pulse, jitter = pulse_helper.get_jitter_pulse()
+        data[i, :] = noises.values[i][:] + np.multiply(A[i], jitter_pulse)
+
+        jitters[i][0] = int(jitter)
+        jitters[i][1:number_of_jitter_info] = jitter_pulse
 
     file_helper.save_file_in(f'tile_signal{sufix}', folder_name, data)
     file_helper.save_file_in(f'tile_A{sufix}', folder_name, A)
+    _save_jitter_file(f'jitter{sufix}', folder_name, jitters)
 
 
 if __name__ == '__main__':

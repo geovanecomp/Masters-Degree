507a1ff20c5cdb407f76c2b940ce5e5dfcbb0930
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-04-06 22:00:51 -0300

    Relative error comparison

---
+++ code/plots/relative_error.py
@@ -0,0 +1,62 @@
+import os.path
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import datashader as ds
+import datashader.transfer_functions as tf
+
+from datashader.mpl_ext import dsshow
+from functools import partial
+
+DIR_PATH = os.path.dirname(__file__)
+
+
+def _relative_error(exact_value, approx_value):
+    return (approx_value - exact_value) / exact_value
+    # return np.absolute((approx_value - exact_value) / exact_value)
+
+
+def correlation_plot(x, y, ax, method):
+    ax.legend()
+    ax.grid(True)
+
+    ax.set_ylabel(f'Erro Relativo {method}')
+    ax.set_xlabel('Amplitude de Referencia')
+    df = pd.DataFrame(dict(x=x, y=y))
+
+    da1 = dsshow(df, ds.Point('x', 'y'), norm='log', aspect='auto', ax=ax, x_range=(-10, 400), y_range=(-50, 50), shade_hook=partial(tf.dynspread, threshold=0.8))
+
+    fig.colorbar(da1, ax=ax).set_label('Densidade')
+    return ax
+
+
+if "__main__" == __name__:
+
+    # Real data
+    noise_mean = 30
+    channel = 36
+    amplitude_mean = 30
+
+    sufix = f'_ch{channel}'
+    base_folder = DIR_PATH + f'/../results/hybrid/amplitude_mean{amplitude_mean}'
+
+    of_amp_file_name = f'{base_folder}/OF/mu{noise_mean}/of_amp_signal{sufix}.txt'
+    dmf_amp_file_name = f'{base_folder}/D_MF/mu{noise_mean}/dmf_amp_signal{sufix}.txt'
+    emf_amp_file_name = f'{base_folder}/E_MF/mu{noise_mean}/mf_amp_signal{sufix}.txt'
+    reference_data_file_name = f'{base_folder}/base_data/mu{noise_mean}/tile_A{sufix}.txt'
+
+    of_data = np.loadtxt(of_amp_file_name)
+    dmf_data = np.loadtxt(dmf_amp_file_name)
+    emf_data = np.loadtxt(emf_amp_file_name)
+    reference_data = np.loadtxt(reference_data_file_name)[:len(of_data)]
+
+    of_error = _relative_error(reference_data, of_data)
+    dmf_error = _relative_error(reference_data, dmf_data)
+    emf_error = _relative_error(reference_data, emf_data)
+
+    fig, (ax0, ax1, ax2) = plt.subplots(3)
+    fig.suptitle(f'Correlação entre Amplitudes \n Ruido: {noise_mean} Canal: {channel} Amplitude: {amplitude_mean}')
+    ax0 = correlation_plot(reference_data, of_error, ax0, method='OF')
+    ax1 = correlation_plot(reference_data, dmf_error, ax1, method='D-MF')
+    ax2 = correlation_plot(reference_data, emf_error, ax2, method='E-MF')
+    plt.show()

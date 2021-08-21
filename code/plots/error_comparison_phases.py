7470b4648dc6f2cceddc9d12688f35c7de4dd088
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-08-21 15:04:20 -0300

    Error phases comparison finished

--- code/plots/error_comparison_phases.py
+++ code/plots/error_comparison_phases.py
@@ -12,14 +12,17 @@ def _get_distinct_jitters(jitters):
 
 def _compute_phase_mean(jitter_value, jitters, errors):
     jitter_errors = []
+    jitter_std_errors = []
     for i in range(len(jitters)):
         if jitter_value == jitters[i]:
             jitter_errors.append(errors[i])
+            jitter_std_errors.append(errors[i]/20)
 
     jitter_mean = np.mean(jitter_errors)
     jitter_std = np.std(jitter_errors)
+    jitter_std_error_bar = np.std(jitter_std_errors)
 
-    return jitter_mean, jitter_std
+    return jitter_mean, jitter_std, jitter_std_error_bar
 
 
 def _compute_relative_mean_per_phase(jitters, errors):
@@ -32,18 +35,19 @@ def _compute_relative_mean_per_phase(jitters, errors):
 
     phase_means = {}
     phase_stds = {}
+    jitter_std_error_bar = {}
     for jitter_value in set(jitter_values):
-        phase_means[jitter_value], phase_stds[jitter_value] = _compute_phase_mean(jitter_value, jitters, errors)
+        phase_means[jitter_value], phase_stds[jitter_value], jitter_std_error_bar[jitter_value] = _compute_phase_mean(jitter_value, jitters, errors)
 
-    return phase_means, phase_stds
+    return phase_means, phase_stds, jitter_std_error_bar
 
 
 if "__main__" == __name__:
 
     # Hybrid data
-    amplitude_mean = 300
-    noise_mean = 90
-    channel = 36
+    amplitude_mean = 10
+    noise_mean = 30
+    channel = 1
 
     sufix = f'_ch{channel}'
     base_folder = DIR_PATH + f'/../results/hybrid/amplitude_mean{amplitude_mean}'
@@ -61,27 +65,26 @@ if "__main__" == __name__:
 
     t0 = time.time()
 
-    phase_means_of, phase_stds_of = _compute_relative_mean_per_phase(jitter_data, of_error)
-    phase_means_dmf, phase_stds_dmf = _compute_relative_mean_per_phase(jitter_data, dmf_error)
-    phase_means_emf, phase_stds_emf = _compute_relative_mean_per_phase(jitter_data, emf_error)
+    phase_means_of, phase_stds_of, std_error_bar_of = _compute_relative_mean_per_phase(jitter_data, of_error)
+    phase_means_dmf, phase_stds_dmf, std_error_bar_dmf = _compute_relative_mean_per_phase(jitter_data, dmf_error)
+    phase_means_emf, phase_stds_emf, std_error_bar_emf = _compute_relative_mean_per_phase(jitter_data, emf_error)
     print(time.time() - t0, "seconds wall time")
 
     fig, ((ax0, ax1)) = plt.subplots(nrows=1, ncols=2)
     fig.suptitle(f'Erro X Fase \n Ruido: {noise_mean} Canal: {channel} Amplitude: {amplitude_mean}')
 
-    # plt.errorbar(phase_means_of.keys(), phase_means_of.values(), c='r', marker='.', yerr=phase_stds_of.values(), label='OF', ls='None')
-    # plt.errorbar(phase_means_dmf.keys(), phase_means_dmf.values(), c='g', marker='x', yerr=phase_stds_dmf.values(), label='D-MF', ls='None')
-    # plt.errorbar(phase_means_emf.keys(), phase_means_emf.values(), c='b', marker='+', yerr=phase_stds_emf.values(), label='E-MF', ls='None')
     ax0.set_title('Média')
-    ax0.plot(phase_means_of.keys(), phase_means_of.values(), 'r.', label='OF')
-    ax0.plot(phase_means_dmf.keys(), phase_means_dmf.values(), 'gx', label='D-MF')
-    ax0.plot(phase_means_emf.keys(), phase_means_emf.values(), 'b+', label='E-MF')
+    ax0.errorbar(phase_means_of.keys(), phase_means_of.values(), c='r', marker='.', yerr=std_error_bar_of.values(), label='OF', ls='None')
+    ax0.errorbar(phase_means_dmf.keys(), phase_means_dmf.values(), c='g', marker='x', yerr=std_error_bar_dmf.values(), label='D-MF', ls='None')
+    ax0.errorbar(phase_means_emf.keys(), phase_means_emf.values(), c='b', marker='+', yerr=std_error_bar_emf.values(), label='E-MF', ls='None')
+    # ax0.plot(phase_means_of.keys(), phase_means_of.values(), 'r.', label='OF')
+    # ax0.plot(phase_means_dmf.keys(), phase_means_dmf.values(), 'gx', label='D-MF')
+    # ax0.plot(phase_means_emf.keys(), phase_means_emf.values(), 'b+', label='E-MF')
     ax0.grid(alpha=0.75)
     ax0.set_xlabel('Fase')
     ax0.set_ylabel('Contagens de ADC')
     ax0.legend()
-    # ax0.set_xlim(-75, 75)
-    # ax0.set_ylim(-500, 500)
+    ax0.set_ylim(-400, 10)
 
     ax1.set_title('RMS')
     ax1.plot(phase_stds_of.keys(), phase_stds_of.values(), 'r.', label='OF')
@@ -91,7 +94,6 @@ if "__main__" == __name__:
     ax1.set_xlabel('Fase')
     ax1.set_ylabel('Contagens de ADC')
     ax1.legend()
-    # ax1.set_xlim(-75, 75)
-    # ax1.set_ylim(-1, 500)
+    ax1.set_ylim(-1, 400)
 
     plt.show()

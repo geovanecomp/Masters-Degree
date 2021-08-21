fca00e8ea81431937825a7bf2b96a5fe639ac09d
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-08-21 19:40:47 -0300

    Updates pu generator

--- code/simulated_pileup_signals_generator.py
+++ code/simulated_pileup_signals_generator.py
@@ -6,6 +6,15 @@ from utils import file_helper, pulse_helper
 TILECAL_NUMBER_OF_CHANNELS = 7
 
 
+def _save_jitter_file(file_name, folder_name, data):
+    jitter_file_fmt = '%d', '%.7f', '%.7f', '%.7f', '%.7f', '%.7f', '%.7f', '%.7f'
+    np.savetxt(
+        f'{folder_name}/{file_name}.txt',
+        data,
+        fmt=jitter_file_fmt,
+        delimiter=' ')
+
+
 # Generates a base data that will be randomized to simulate the real signal
 def _base_data(number_of_data, pedestal):
     mu, sigma = pedestal, 1.5  # Mean and standard deviation
@@ -14,7 +23,6 @@ def _base_data(number_of_data, pedestal):
 
 def _pileup_indexes(signal_probability, number_of_data):
     pu_indexes = np.random.permutation(number_of_data)
-    # What about when "signal_probability" is float?
     pu_indexes = pu_indexes[0:int(signal_probability * number_of_data)]
     return pu_indexes
 
@@ -24,14 +32,16 @@ def _pileup():
     return np.random.exponential(pileup_mean)
 
 
-# pileup should be added in the position "i" and then in its corners.
+# Pileup should be added in the position "i" and then in its corners.
 # for exemple, for n=100, i=97, and a 7th dimension signal we will have pileup
 # added at positions 94, 95, 96, 97, 98, 99, 100
 # TODO: Improve these magic numbers.
-def _apply_pileup_indexes_when_tilecal(i, pu_indexes, x):
-    pu = np.multiply(_pileup(), pulse_helper.get_jitter_pulse())
+def _apply_pileup_indexes(i, pu_indexes, x):
+    jitter_pulse, _ = pulse_helper.get_jitter_pulse()
+    pu = np.multiply(_pileup(), jitter_pulse)
     number_of_data = len(x)
 
+    # Start checking ther corners
     if pu_indexes[i] < 4:
         for j in range(pu_indexes[i] - 2, 3):
             x[pu_indexes[i] + j] = x[pu_indexes[i] + j] + pu[j + 4]
@@ -45,25 +55,11 @@ def _apply_pileup_indexes_when_tilecal(i, pu_indexes, x):
     return x
 
 
-def _apply_pileup_indexes(i, pu_indexes, x):
-    pu = np.multiply(_pileup(), pulse_helper.get_pulse_paper_COF())
-    number_of_data = len(x)
-
-    if pu_indexes[i] < 3:
-        for j in range(pu_indexes[i]-2, 7):
-            x[pu_indexes[i] + j] = x[pu_indexes[i] + j] + pu[j + 3]
-    elif pu_indexes[i] > 999993:
-        for j in range(-3, number_of_data - pu_indexes[i]):
-            x[pu_indexes[i] + j] = x[pu_indexes[i] + j] + pu[j + 3]
-    else:
-        for j in range(-3, 7):
-            x[pu_indexes[i] + j] = x[pu_indexes[i] + j] + pu[j + 3]
-    return x
-
-
-def pu_generator(number_of_events, signal_probabilities, pedestal, is_noise=False):
+def pu_generator(number_of_events, signal_probabilities, pedestal):
     number_of_data = TILECAL_NUMBER_OF_CHANNELS * number_of_events
     base_folder = 'results/simulated/pileup_data'
+    number_of_jitter_info = TILECAL_NUMBER_OF_CHANNELS + 1
+    jitters = np.zeros((number_of_events, number_of_jitter_info))
 
     for level in range(0, len(signal_probabilities)):
         signal_probability = signal_probabilities[level]  # Signal_probability
@@ -77,27 +73,32 @@ def pu_generator(number_of_events, signal_probabilities, pedestal, is_noise=Fals
 
         if signal_probability > 0:
             for i in range(0, int(signal_probability * number_of_data)):
-                x = _apply_pileup_indexes_when_tilecal(i, pu_indexes, x)
+                x = _apply_pileup_indexes(i, pu_indexes, x)
 
-        # Formatting data
-        data = np.reshape(x, (TILECAL_NUMBER_OF_CHANNELS, number_of_events))
-        data = np.transpose(data)
+        # Formatting data to the tilecal shape (nx7)
+        pu_data = np.reshape(x, (TILECAL_NUMBER_OF_CHANNELS, number_of_events))
+        pu_data = np.transpose(pu_data)
+        pu_signals = np.empty_like(pu_data)
 
         # Stores Noise Data
-        folder_name = f'{base_folder}/prob_{signal_probability_percentage}'
-        base_file_name = f'noise_prob_{signal_probability_percentage}'
-        file_helper.save_file('tile_' + base_file_name, folder_name, data)
+        folder_name = f'{base_folder}/prob_{signal_probability_percentage}/{number_of_events}_events'
+        file_helper.save_file_in(f'noise_{signal_probability_percentage}', folder_name, pu_data)
 
-        # Stores mixed signal and amplitude
-        folder_name = f'{base_folder}/prob_{signal_probability_percentage}'
-        base_file_name = f'signal_prob_{signal_probability_percentage}'
         A = np.zeros(number_of_events)  # Amplitude
         for i in range(0, number_of_events):
             A[i] = np.random.exponential(signal_mean)  # Simulating true Amplitude
-            data[i, :] = data[i, :] + np.multiply(A[i], pulse_helper.get_jitter_pulse())
+            jitter_pulse, jitter = pulse_helper.get_jitter_pulse()
+            pu_signals[i, :] = pu_data[i, :] + np.multiply(A[i], jitter_pulse)
+            jitters[i][0] = int(jitter)
+            jitters[i][1:number_of_jitter_info] = jitter_pulse
 
-        file_helper.save_file('tile_' + base_file_name, folder_name, data)
-        file_helper.save_file('tile_A_' + base_file_name, folder_name, A)
+        # Stores mixed signal and amplitude
+        prob_file_name = f'_prob_{signal_probability_percentage}'
+        file_helper.save_file_in(f'tile_signal{prob_file_name}', folder_name, pu_signals)
+        file_helper.save_file_in(f'tile_A{prob_file_name}', folder_name, A)
+
+        # Its important to store jitter and use the same values across methods
+        _save_jitter_file(f'jitter{prob_file_name}', folder_name, jitters)
         level += 1
 
 
@@ -107,7 +108,7 @@ if __name__ == '__main__':
     # We can use an array to generate signas for several probabilities.
     # signal_probabilities = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     signal_probabilities = [0.0, 0.5, 1.0]
-    number_of_events = 10000
+    number_of_events = 100
     pedestal = 0
 
-    pu_generator(number_of_events, signal_probabilities, pedestal, is_noise=False)
+    pu_generator(number_of_events, signal_probabilities, pedestal)

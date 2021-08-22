0a7193b2cb712bce6a9fb020f4f3b15942dc69c1
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-08-21 22:17:33 -0300

    Updates amp definition and location

--- code/hybrid_signals_generator.py
+++ code/hybrid_signals_generator.py
@@ -14,9 +14,8 @@ def _save_jitter_file(file_name, folder_name, data):
         delimiter=' ')
 
 
-def signals_generator(noises, noise_mean, sufix=''):
+def signals_generator(amp_mean, noises, noise_mean, sufix=''):
     dimension = 7
-    amplitude_mean = 30  # Exponential signal mean
     number_of_events = len(noises)
     number_of_jitter_info = dimension + 1
     print(f'Hybrid Signal Generator amp{amplitude_mean} using mean: {noise_mean}{sufix}')
@@ -40,10 +39,14 @@ def signals_generator(noises, noise_mean, sufix=''):
 
 
 if __name__ == '__main__':
+    # 10 ADC when converted to MeV it is equal to 100MeV = 0.1GeV
+    # In this case we are considering 1ADC=10MeV (instead of 12MeV)
+    amplitude_mean = 10  # Exponential signal mean
+    channel = 1
     noise_mean = 30
-    channel = 10
+
     sufix = f'_ch{channel}'
     tile_partition = 'EBA'
     noise_file_name = f'data/{tile_partition}/{tile_partition}mu{noise_mean}_no_ped{sufix}.txt'
     noises = pd.read_csv(noise_file_name, sep=" ", usecols=(3, 4, 5, 6, 7, 8, 9), header=None)
-    signals_generator(noises, noise_mean, sufix)
+    signals_generator(amp_mean, noises, noise_mean, sufix)

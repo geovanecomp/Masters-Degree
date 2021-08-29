2dc9544764a38913b94307c1002ad4ecfd66a644
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-08-29 12:05:01 -0300

    Fix amount of data for processing

--- code/simulated_signals_deterministic_matched_filter.py
+++ code/simulated_signals_deterministic_matched_filter.py
@@ -7,7 +7,7 @@ from utils import file_helper
 
 def deterministic_matched_filter(number_of_events, probs):
     base_folder = 'results/simulated/pileup_data'
-    number_of_data = number_of_events * 2  # Due to the 50% training of E-MF.
+    number_of_data = number_of_events * 2  # Due to the 50% training of S-MF.
 
     # For printing and files, probability must be in %.
     probs = np.array(probs) * 100
@@ -24,9 +24,9 @@ def deterministic_matched_filter(number_of_events, probs):
         # Getting data from boundaries
         all_noises = pd.read_csv(noise_file_name, sep=" ", header=None)
 
-        amplitude = pd.read_csv(amplitude_file_name, sep=" ", header=None)[:number_of_data]
-        signal_testing = pd.read_csv(signal_file_name, sep=" ", header=None)[:number_of_data][:]
-        noises = all_noises[:number_of_data][:]
+        amplitude = pd.read_csv(amplitude_file_name, sep=" ", header=None)[:number_of_events]
+        signal_testing = pd.read_csv(signal_file_name, sep=" ", header=None)[:number_of_events][:]
+        noises = all_noises[:number_of_events][:]
 
         S = pd.DataFrame([0, 0.0172, 0.4524, 1.0000, 0.5633, 0.1493, 0.0424])
 

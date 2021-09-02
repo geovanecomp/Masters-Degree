2e8e891f5414fe6d4dc4896c9bc6e1a5785ce17a
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-09-02 11:11:17 -0300

    New fonts to pileups comparison

--- code/plots/simulated/pileups_comparison.py
+++ code/plots/simulated/pileups_comparison.py
@@ -3,10 +3,19 @@
 import os.path
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from config import Legend, Tick
 
 DIR_PATH = os.path.dirname(__file__)
 
 if __name__ == '__main__':
+    dataset = 'simulated_snr01'
     num_events = 200000
     probs = [0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0]
 
@@ -19,7 +28,7 @@ if __name__ == '__main__':
     smf_stds = []
     smf_means = []
     for prob in probs:
-        common_path = DIR_PATH + f'/../../results/simulated/pileup_data/prob_{prob}/{num_events}_events'
+        common_path = DIR_PATH + f'/../../results/{dataset}/pileup_data/prob_{prob}/{num_events}_events'
 
         of_error_file_name = common_path + '/OF/of_amp_error.txt'
         dmf_error_file_name = common_path + '/D_MF/dmf_amp_error.txt'
@@ -40,14 +49,14 @@ if __name__ == '__main__':
 
     fig, ((ax0, ax1)) = plt.subplots(nrows=1, ncols=2)
 
-    fig.suptitle('OF X DMF X SMF' ' {} eventos'
+    fig.suptitle('OF X DMF X SMF' ' {} eventos SNR = 0,1'
                  .format(num_events))
 
     ax0.legend(prop={'size': 10})
     ax0.grid(axis='y', alpha=0.75)
     ax0.set_title('Média')
-    ax0.set_xlabel('Empilhamento (%)')
-    ax0.set_ylabel('Média')
+    ax0.set_xlabel('Empilhamento (%)', **Legend.font)
+    ax0.set_ylabel('Média', **Legend.font)
     ax0.plot(probs, of_means, '-ro', label='OF')
     ax0.plot(probs, dmf_means, '-bo', label='DMF')
     ax0.plot(probs, smf_means, '-go', label='SMF')
@@ -57,8 +66,8 @@ if __name__ == '__main__':
     ax1.legend(prop={'size': 10})
     ax1.grid(axis='y', alpha=0.75)
     ax1.set_title('Desvio Padrão')
-    ax1.set_xlabel('Empilhamento (%)')
-    ax1.set_ylabel('Desvio Padrão')
+    ax1.set_xlabel('Empilhamento (%)', **Legend.font)
+    ax1.set_ylabel('Desvio Padrão', **Legend.font)
     ax1.plot(probs, of_stds, '-ro', label='OF')
     ax1.plot(probs, dmf_stds, '-bo', label='DMF')
     ax1.plot(probs, smf_stds, '-go', label='SMF')

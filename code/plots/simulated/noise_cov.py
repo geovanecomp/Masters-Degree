b7383f80c3812896b9bc0812db5d4bfe9d746094
Geovane Pacheco <geovane.pacheco99@gmail.com>
2021-09-07 11:53:04 -0300

    Covariance matrix for simulated noises

--- code/plots/simulated/noise_cov.py
+++ code/plots/simulated/noise_cov.py
@@ -3,6 +3,7 @@
 import os.path
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mp
 import sys
 import inspect
 
@@ -10,13 +11,15 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
+from config import Legend, Tick
+
 DIR_PATH = os.path.dirname(__file__)
 
 if __name__ == '__main__':
     np.set_printoptions(suppress=True, precision=3)
-    prob = 100.0
-    dataset = 'simulated'
-    num_events = 20000
+    prob = 0.0
+    dataset = 'simulated_snr3'
+    num_events = 200000
 
     # Pile data
     BASE_PATH = DIR_PATH + f'/../../results/{dataset}/pileup_data/prob_{prob}/{num_events}_events'
@@ -24,13 +27,20 @@ if __name__ == '__main__':
 
     noises = np.loadtxt(noise_file_name)
 
+    cov = np.cov(np.transpose(noises))
+    plt.matshow(cov, cmap='gray_r')
+    plt.gca().xaxis.tick_bottom()
+
     print('Noises')
     print(np.mean(noises))
     print(np.std(noises))
-
-    cov = np.cov(np.transpose(noises))
-    plt.matshow(cov)
     print(cov)
-    plt.gray()
-    plt.colorbar()
+
+    cbar = plt.colorbar()
+    tick_font_size = 14
+    cbar.ax.tick_params(labelsize=tick_font_size)
+    plt.xlabel('Amostra Temporal', **Legend.font)
+    plt.ylabel('Amostra Temporal', **Legend.font)
+    plt.xticks(**Tick.font)
+    plt.yticks(**Tick.font)
     plt.show()
